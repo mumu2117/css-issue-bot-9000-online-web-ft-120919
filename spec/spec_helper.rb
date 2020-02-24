84ec72595a5a32d7fa5892805bb6a76c18804010
Jennifer(Lakshmi) Miller <jmillerarcher@me.com>
2020-02-24 02:08:12 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,23 @@
-require 'bundler'
-Bundler.require
-require 'yaml'
+# require 'bundler'
+# Bundler.require
+# require 'yaml'
 
-RSpec.configure do |config|
+# RSpec.configure do |config|
   
-end
+# end
 
-def html_file_contents
-  File.read('./index.html')
-end
+# def html_file_contents
+#   File.read('./index.html')
+# end
 
-def parsed_html
-  Nokogiri::HTML(html_file_contents) do |config|
-    config.strict.dtdload.dtdvalid.noblanks
-  end
-end
+# def parsed_html
+#   Nokogiri::HTML(html_file_contents) do |config|
+#     config.strict.dtdload.dtdvalid.noblanks
+#   end
+# end
 
-def parsed_css
-  parser = CssParser::Parser.new
-  parser.load_uri!('./css/style.css')
-  parser
-end
+# def parsed_css
+#   parser = CssParser::Parser.new
+#   parser.load_uri!('./css/style.css')
+#   parser
+# end
